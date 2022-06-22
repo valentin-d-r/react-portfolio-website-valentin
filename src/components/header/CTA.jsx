9f20a0205d1786871530336fa44a08b056dc19c7
Valentin <valentin.dremaux@viacesi.fr>
2022-06-22 09:33:21 +0200

    Add the PFI in the page

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CVi from "../../assets/Valentin_Dremaux_Resume.pdf";
 import CVfr from "../../assets/Valentin_Dremaux_CV.pdf";
+import PFI from "../../assets/PFI.pdf";
 
 const CTA = () => {
   return (
@@ -10,6 +11,9 @@ const CTA = () => {
       </a>
       <a href={CVfr} className='btn'>
         Download CV (France)
+      </a>
+      <a href={PFI} className='btn'>
+        Download PFI
       </a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
